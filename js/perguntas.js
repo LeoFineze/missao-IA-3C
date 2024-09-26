2a0140baa1f389afd5754f2180ad1545d3bb86c2
LeoFineze <128715920+LeoFineze@users.noreply.github.com>
2024-09-26 07:52:36 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,61 @@
+export const perguntas = [
+    {
+        enunciado: "Você gosta de futebol?",
+        alternativas: [
+            {
+                texto: "Sim, eu gosto",
+                afirmacao: [
+                    "Que legal, você gosta mesmo de futebol",
+                    "Que pena, parece que você não gosta de futebol"
+                    ]
+            },
+            {
+                texto: "Não,eu detesto",
+                afirmacao: [
+                    "Que pena, parece que você não gosta de futebol",
+                    "Que legal, você gosta mesmo de futebol"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "você gosta de correr?",
+        alternativas: [
+            {
+                texto:"Sim, eu gosto",
+                afirmacao: [
+                    "que legal, você gosta de correr",
+                    "Não, detesto correr"
+                    ]
+            },
+            {
+                texto: "Não, detesto correr",
+                afirmacao: [
+                    "que legal, você gosta de correr",
+                    "afirmacao 2"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você gosta de playstation?",
+        alternativas: [
+            {
+                texto:"Sim, é um ótimo console",
+                afirmacao: [
+                    "Que legal,eu concordo",
+                    "Entendi, cada um tem sua preferência"
+                    ]
+            },
+            {
+                texto:"Não, eu prefiro outro console",
+                afirmacao: [
+                    "Que legal,eu concordo",
+                    "Entendi, cada um tem sua preferência"
+                    ]
+            }
+            
+        ]
+    },
+];
