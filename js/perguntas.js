5fefbbefc42f2463bb3b7fc30c578c8f7b4a7d24
LeoFineze <128715920+LeoFineze@users.noreply.github.com>
2024-10-21 21:27:10 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,61 +1,120 @@
 export const perguntas = [
     {
-        enunciado: "Você gosta de futebol?",
+        enunciado: "Você assiste gameplay de algum youtuber ?",
         alternativas: [
             {
-                texto: "Sim, eu gosto",
+                texto: "Sim",
                 afirmacao: [
-                    "Que legal, você gosta mesmo de futebol",
-                    "Que pena, parece que você não gosta de futebol"
-                    ]
+                    "Ao assistir, ele está sendo um inútil"
+                    ],
+                proxima: 1,
             },
             {
-                texto: "Não,eu detesto",
+                texto: "Não",
                 afirmacao: [
-                    "Que pena, parece que você não gosta de futebol",
-                    "Que legal, você gosta mesmo de futebol"
-                    ]
-            }           
-            
+                    "Ao não assistir, ele está sendo produtivo em porcaria nenhuma"
+                    ],
+                proxima: 1, 
+            },
         ]
     },
+    
     {
-        enunciado: "você gosta de correr?",
+        enunciado: "Você gosta de jogar jogos de terror ?",
         alternativas: [
             {
-                texto:"Sim, eu gosto",
+                texto:"Sim",
                 afirmacao: [
-                    "que legal, você gosta de correr",
-                    "Não, detesto correr"
-                    ]
+                    " e que ao gostar de jogo de terror ele não tem medo. "
+                    ],
+                proxima: 2, 
             },
             {
-                texto: "Não, detesto correr",
+                texto: "Não, tenho medo",
                 afirmacao: [
-                    "que legal, você gosta de correr",
-                    "Não, detesto correr"
-                    ]
-            }
+                    " e que ao gostar de jogo de terror ele tem medo. "
+                    ],
+                proxima: 2, 
+            },
         ]
     },
+    
     {
-        enunciado: "Você gosta de playstation?",
+        enunciado: "Você prefere que tipo de plataforma ?",
         alternativas: [
             {
-                texto:"Sim, é um ótimo console",
+                texto:"Consoles",
                 afirmacao: [
-                    "Que legal,eu concordo",
-                    "Entendi, cada um tem sua preferência"
+                    "Quando ele escolhe console ele mostra o quão burro ele é,"
+                    ],
+                proxima: 3, 
+            },
+            {
+                texto:"Computador",
+                afirmacao: [
+                    "Quando ele escolhe PC ele mostra o quão sábio ele é, "
+                ],
+                proxima: 3, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "Você acha justo o preço cobrados nos jogos ?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "e que quando ele acha o preço dos jogos justo, ele mostra o quão burro financeiramente é"
+                    ],
+                proxima: 4, 
+            },
+            {
+                texto: "Não, eu sou inteligente",
+                afirmacao: [
+                    "e que quando ele acha o preço dos jogos justo, ele mostra o quão burro financeiramente é"
+                ],
+                proxima: 4, 
+            },     
+        ]
+    },
+    
+    {
+        enunciado: "Você é a favor da pirataria ?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    ". O cara lembrou que mora no Brasil "
+                    ],
+                proxima: 5, 
+            },
+            {
+                texto: "Não, eu sou um bosta",
+                afirmacao: [
+                    ". O cara esqueceu que mora no Brasil "
+                    ],
+                proxima: 5, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "Você gostaria de trabalhar com a programação de jogos ?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    " mal sabia ele(a) que irá passar fome"
                     ]
             },
             {
-                texto:"Não, eu prefiro outro console",
+                texto: "Não",
                 afirmacao: [
-                    "Que legal,eu concordo",
-                    "Entendi, cada um tem sua preferência"
-                    ]
-            }
-            
+                    " mal sabia ele que escapou da fome, amém !"
+                    ] 
+            },   
         ]
     },
 ];
+
